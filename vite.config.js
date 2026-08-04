1a255c989c560d801e00f22a64a4ad76d26f334d
farseen <farseen247@gmail.com>
2026-08-04 12:47:33 +0530

    hosted in local network

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: '0.0.0.0', // Listen on all network interfaces
+    port: 5173,      // Optional
+  },
 })
